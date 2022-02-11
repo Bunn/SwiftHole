8aa97c8348333ac976bb5306665d80501995dbb4
Gustavo <66035402+gpnunes75@users.noreply.github.com>
2022-02-11 10:17:40 -0300

    Add files via upload
    Reverse Proxy compatibilty. I have changed methods using default values (false), so it is not supposed to broke other projects

---
+++ Sources/SwiftHole/Router.swift
@@ -0,0 +1,95 @@
+//
+//  File.swift
+//  
+//
+//  Created by Fernando Bunn on 25/04/2020.
+//
+
+import Foundation
+
+internal enum Router {
+    case getSummary(Environment)
+    case getLogs(Environment)
+    case disable(Environment, Int)
+    case enable(Environment)
+    case getHistoricalQueries(Environment)
+    
+    var scheme: String {
+        switch self {
+        case .getSummary(let environment),
+             .getLogs (let environment),
+             .disable(let environment, _),
+             .enable (let environment),
+             .getHistoricalQueries(let environment):
+            return environment.secure ? "https" : "http"
+        }
+    }
+    
+    var host: String {
+        switch self {
+        case .getSummary(let environment),
+             .getLogs (let environment),
+             .disable(let environment, _),
+             .enable (let environment),
+             .getHistoricalQueries(let environment):
+            return environment.host
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getSummary(let environment),
+             .getLogs(let environment),
+             .disable(let environment, _),
+             .enable(let environment),
+             .getHistoricalQueries(let environment):
+            return environment.reverserproxy ? "/api.php" : "/admin/api.php"
+        }
+    }
+
+
+    var port: Int? {
+        switch self {
+        case .getSummary(let environment),
+             .getLogs (let environment),
+             .disable(let environment, _),
+             .enable (let environment),
+             .getHistoricalQueries (let environment):
+            return environment.port        
+        }
+    }
+    
+    var method: String {
+        switch self {
+        case .getSummary,
+             .getLogs,
+             .disable,
+             .enable,
+             .getHistoricalQueries:
+            return "GET"
+        }
+    }
+    
+    var parameters: [URLQueryItem] {
+        switch self {
+        case .getSummary:
+            return []
+            
+        case .getLogs (let environment):
+            return [URLQueryItem(name: "getAllQueries", value: "100"),
+                    URLQueryItem(name: "auth", value: environment.apiToken ?? "")]
+            
+        case .disable(let environment, let seconds):
+            return [URLQueryItem(name: "auth", value: environment.apiToken ?? ""),
+                    URLQueryItem(name: "disable", value: "\(seconds)")]
+            
+        case .enable(let environment):
+            return [URLQueryItem(name: "auth", value: environment.apiToken ?? ""),
+                    URLQueryItem(name: "enable", value: "")]
+            
+        case .getHistoricalQueries (let environment):
+            return [URLQueryItem(name: "overTimeData10mins", value: ""),
+                    URLQueryItem(name: "auth", value: environment.apiToken ?? "")]
+        }
+    }
+}
--- Sources/SwiftHole/SwiftHole.swift
+++ Sources/SwiftHole/SwiftHole.swift
@@ -7,7 +7,7 @@ private enum EnableDisableMethodType {
 
 public struct SwiftHole {
     private let environment: Environment
-    private var service: ServiceProtocol
+    private var service = Service()
     
     public var timeoutInterval: TimeInterval {
         set {
@@ -21,32 +21,15 @@ public struct SwiftHole {
     
     // MARK: Public Methods
     
-    /// Initialize SwiftHole
-    /// - Parameters:
-    ///   - host: Host IP Address
-    ///   - port: Optional host port
-    ///   - apiToken: Optional API Token, required for some methods like disable/enable
-    ///   - timeoutInterval: Interval for timeout
-    ///   - secure: Boolean to indicate if the pi-hole is using HTTPS or not
-    public init(host: String, port: Int? = nil, apiToken: String? = nil, timeoutInterval: TimeInterval = 30, secure: Bool = false) {
-        self.service = Service()
-        self.service.timeoutInterval = timeoutInterval
-        environment = Environment(host: host, port: port, apiToken: apiToken, secure: secure)
+    public init(host: String, port: Int? = nil, apiToken: String? = nil, timeoutInterval: TimeInterval = 30, secure: Bool = false, reverserproxy: Bool = false) {
+        service.timeoutInterval = timeoutInterval
+        environment = Environment(host: host, port: port, apiToken: apiToken, secure: secure, reverserproxy: reverserproxy)
     }
     
-    //Init method that receives a ServiceProtocol for testing
-    init(host: String, port: Int? = nil, apiToken: String? = nil, timeoutInterval: TimeInterval = 30, secure: Bool = false, service: ServiceProtocol) {
-        self.service = service
-        self.service.timeoutInterval = timeoutInterval
-        environment = Environment(host: host, port: port, apiToken: apiToken, secure: secure)
-    }
-    
-    /// Fetches pi-hole summary and assigns it to its summary property
     public func fetchSummary(completion: @escaping (Result<Summary, SwiftHoleError>) -> ()) {
         service.request(router: .getSummary(environment), completion: completion)
     }
     
-    /// Enable pi-hole. Does nothing if pi-hole is already enabled
     public func enablePiHole(_ completion: @escaping (Result<Void, SwiftHoleError>) -> ()) {
         if environment.apiToken == nil {
             completion(.failure(.noAPITokenProvided))
@@ -58,10 +41,7 @@ public struct SwiftHole {
         }
     }
     
-    
-    /// Disable pi-hole.
-    /// - Parameters:
-    ///   - seconds: Number in seconds for the pi-hole to get back online once disabled. 0 seconds means it will be disabled permanently
+    //0 seconds means PiHole will be disabled permanently
     public func disablePiHole(seconds: Int = 0, completion: @escaping (Result<Void, SwiftHoleError>) -> ()) {
         if environment.apiToken == nil {
             completion(.failure(.noAPITokenProvided))
@@ -73,78 +53,11 @@ public struct SwiftHole {
         }
     }
     
-    
-    /// Fetches the pi-hole black or white lists
-    /// - Parameters:
-    ///   - listType: Type of list you want to fetch
-    public func fetchList(_ listType: ListType, completion: @escaping (Result<[ListItem], SwiftHoleError>) -> ()) {
-        service.request(router: .getList(environment, listType)) { (result: Result<List, SwiftHoleError>) in
-            switch result {
-            case .success(let list):
-                completion(.success(list.items))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    
-    /// Fetches the amount of queries made over time
     public func fetchHistoricalQueries(completion: @escaping (Result<[DNSRequest], SwiftHoleError>) -> ()) {
         service.request(router: .getHistoricalQueries(environment)) { (result: Result<HistoricalQueries, SwiftHoleError>) in
             switch result {
             case .success(let historicalQueries):
                 completion(.success(historicalQueries.requests))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    /// Add a domain to a list
-    /// - Parameters:
-    ///   - domain: Domain address
-    ///   - list: List type.
-    public func add(domain: String, to list: ListType, completion: @escaping (Result<Void, SwiftHoleError>) -> ()) {
-        service.request(router: .addToList(environment, list, domain)) { (result: Result<EditListResponse, SwiftHoleError>) in
-            switch result {
-            case .success(let response):
-                if response.success {
-                    completion(.success(()))
-                } else {
-                    completion(.failure(.cantAddNewListItem(response.message ?? "Unknown Error")))
-                }
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    /// Remove a domain to a list
-    /// - Parameters:
-    ///   - domain: Domain address
-    ///   - list: List type.
-    public func remove(domain: String, from list: ListType, completion: @escaping (Result<Void, SwiftHoleError>) -> ()) {        
-        service.request(router: .removeFromList(environment, list, domain)) { (result: Result<EditListResponse, SwiftHoleError>) in
-            switch result {
-            case .success(let response):
-                if response.success {
-                    completion(.success(()))
-                } else {
-                    completion(.failure(.cantAddNewListItem(response.message ?? "Unknown Error")))
-                }
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    /// Fetches the percentage of queries type made
-    public func fetchQueryTypePercentages(completion: @escaping (Result<[QueryPercentage], SwiftHoleError>) -> ()) {
-        service.request(router: .getQueryTypes(environment)) { (result: Result<QueryPercentage.List, SwiftHoleError>) in
-            switch result {
-            case .success(let list):
-                completion(.success(list.values))
             case .failure(let error):
                 completion(.failure(error))
             }
